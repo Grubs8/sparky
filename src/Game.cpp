340b4593b97fe97bac3801d4c1526e5bf49581fe
Grubs <ajgibb89@gmail.com>
2026-07-21 01:00:46 -0500

    modified process input, displayed a window and a quit function

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 Game::Game() {
-	// TODO...
+	isRunning = false;
 	std::cout << "Game constructor called!" << std::endl;
 }
 
@@ -16,7 +16,7 @@ void Game::Initialize() {
 		std::cerr << "Error initializing SDL." << std::endl;
 		return;
 	}
-	SDL_Window* window = SDL_CreateWindow(
+	window = SDL_CreateWindow(
 			NULL, 
 			SDL_WINDOWPOS_CENTERED, 
 			SDL_WINDOWPOS_CENTERED,
@@ -27,15 +27,16 @@ void Game::Initialize() {
 		std::cerr << "Error creating SDL window." << std::endl;
 		return;
 	}
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer) {
 		std::cerr << "Error creating SDL renderer." << std::endl;
 		return;
 	}
+	isRunning = true;
 }
 
 void Game::Run() {
-	while (true){
+	while (isRunning){
 		ProcessInput();
 		Update();
 		Render();
@@ -43,19 +44,37 @@ void Game::Run() {
 }
 
 void Game::ProcessInput() {
-
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent)){
+		switch (sdlEvent.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+					isRunning = false;
+				break;
+				}
+	
+		}
+	}
 }
-
 void Game::Update() {
-
+	//TODO: Update game objects
 }
 
 void Game::Render() {
+	SDL_SetRenderDrawColor(renderer, 255,0,0, 255);
+	SDL_RenderClear(renderer);
 
+	//TODO: Render all game objects
+	SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy(){ 
-
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 
